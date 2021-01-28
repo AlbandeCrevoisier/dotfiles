37d0cdca42d4c0fab917b9f1c5ab8d4b2e507155
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-01-28 18:47:47 +0100

    Update dwm port.

--- ports/dwm/config.h
+++ ports/dwm/config.h
@@ -3,18 +3,19 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Dejavu Sans Mono:size=12" };
-static const char dmenufont[]       = "Dejavu Sans Mono:size=12";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#212121";
-static const char col_gray3[]       = "#424242";
-static const char col_gray4[]       = "#888888";
+static const char *fonts[]          = { "DejaVuSansMono:size=10" };
+static const char dmenufont[]       = "DejaVuSansMono:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_gray3  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -26,7 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "dillo",    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -44,7 +46,7 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ Mod1Mask|ControlMask,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
@@ -54,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray2, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
@@ -82,19 +84,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_h,                      0)
-	TAGKEYS(                        XK_t,                      1)
-	TAGKEYS(                        XK_n,                      2)
-	TAGKEYS(                        XK_s,                      3)
-//	TAGKEYS(                        XK_ampersand,              0)
-//	TAGKEYS(                        XK_bracketleft,            1)
-//	TAGKEYS(                        XK_braceleft,              2)
-//	TAGKEYS(                        XK_braceright,             3)
-	TAGKEYS(                        XK_parenleft,              4)
-	TAGKEYS(                        XK_equal,                  5)
-	TAGKEYS(                        XK_asterisk,               6)
-	TAGKEYS(                        XK_parenright,             7)
-	TAGKEYS(                        XK_plus,                   8)
+	TAGKEYS(                        XK_ampersand,                      0)
+	TAGKEYS(                        XK_bracketleft,                      1)
+	TAGKEYS(                        XK_braceleft,                      2)
+	TAGKEYS(                        XK_braceright,                      3)
+	TAGKEYS(                        XK_parenleft,                      4)
+	TAGKEYS(                        XK_equal,                      5)
+	TAGKEYS(                        XK_asterisk,                      6)
+	TAGKEYS(                        XK_parenright,                      7)
+	TAGKEYS(                        XK_plus,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
@@ -114,3 +112,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
